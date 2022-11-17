2e42c82be46b1c003f87acdcb7e1087eb67ecbfd
david l euler <suibwx@163.com>
2022-11-17 15:37:04 +0800

    build c++ lib against c++17

--- build.rs
+++ build.rs
@@ -3,5 +3,6 @@ fn main() {
     // generate lib.rs.h and lib.rs.cc
     // minimal example: no C++ code to be called from Rust
     cxx_build::bridge("src/lib.rs")
+        .flag_if_supported("-std=c++17")
         .compile("cpp_from_rust");
 }
