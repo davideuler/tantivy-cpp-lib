cbc70531d137169f39e183508188b21441db2164
david l euler <suibwx@163.com>
2022-11-17 15:17:45 +0800

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/*
+  Building this program happens outside of the cargo process.
+  We simply need to link against the Rust library and the
+  system libraries it depends upon
+
+  g++ -std=c++17 -o cpp_program src/main.cpp \
+      -I .. -I target/cxxbridge \
+      -L target/debug -l cpprust \
+      -pthread -l dl
+*/
+
+// consider the ffi part of Rust code
+#include "src/lib.rs.h"
+
+#include <iostream>
+
+int
+main()
+{
+  std::cout << "starting from C++\n";
+  rust_from_cpp();
+  std::cout << "finishing with C++\n";
+  return 0;
+}
